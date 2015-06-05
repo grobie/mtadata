ab9aa672200c0b7d99d75d8b3b220ef7ff28675b
Tobias Schmidt <ts@soundcloud.com>
2015-06-05 00:48:34 -0400

    Publish

--- mtadata.js
+++ mtadata.js
@@ -12,18 +12,39 @@ $(document).ready(function() {
           }
   }
 
+
+
   jQuery.getJSON('data/mta-stations.geojson', function(data){
-          var stations = data.features;
+    var stations = data.features;
+    var lastTrafficKeys = Object.keys(stations[stations.length-1].properties.traffic);
+    var timeMax = parseInt(lastTrafficKeys[lastTrafficKeys.length-1]);
+    var timeMin = parseInt(Object.keys(stations[0].properties.traffic)[0]);
 
-          for(var i = 0; i < stations.length; i++){
-              var properties = stations[i].properties;
+    function setStations(time){
+      for(var i = 0; i < stations.length; i++){
+            var properties = stations[i].properties;
 
-              if(properties.traffic[time]){
-                  properties['marker-color'] = stationColor(properties.traffic[time]);
-              }
-              properties['marker-symbol'] = 'rail-metro';
+            if(properties.traffic[time]){
+                properties['marker-color'] = stationColor(properties.traffic[time]);
+            }
+            properties['marker-symbol'] = 'rail-metro';
           }
+          map.featureLayer.setGeoJSON(stations); 
+    }
 
-          map.featureLayer.setGeoJSON(stations);
+    setStations(timeMin.toString());
+
+    $( "#slider" ).slider({
+      min: timeMin,
+      max: timeMax,
+      step: 3600,
+      slide: function(event, ui){
+        var currentTime = ui.value.toString();
+        setStations(currentTime);
+      }
+    });
   });
+
 });
+
+
